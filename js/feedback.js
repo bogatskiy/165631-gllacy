bcbbb27174d39301cacbd6b97d2c4acef5ae6b18
bogatskiy <a.bogatskiy@mail.ru>
2016-05-14 17:40:08 +0300

    правки

--- js/feedback.js
+++ js/feedback.js
@@ -1,46 +1,54 @@
+var link = document.querySelector(".adderess-block_btn");
+var popup = document.querySelector(".feedback");
+var overlay = document.querySelector(".feedback__overlay");
 
-      var link = document.querySelector(".adderess-block_btn");
-      var popup = document.querySelector(".feedback");
-      var overlay = document.querySelector(".feedback__overlay");
-      var name = document.querySelector(".feedback__user-name");
-      var email = document.querySelector(".feedback__user-email");
+var userName = document.getElementById("feedback-name");
+var userEmail = document.getElementById("feedback-email");
+var userMessage = document.getElementById("feedback-message");
 
-      var close = popup.querySelector(".feedback__close");
-      var form = popup.querySelector("form");
+var close = popup.querySelector(".feedback__close");
+var form = popup.querySelector("form");
 
-      var storage = localStorage.getItem("name");
+link.addEventListener("click", function (event) {
+	event.preventDefault();
+	popup.classList.add("feedback-show");
+	overlay.classList.add("feedback__overlay-show");
 
-      link.addEventListener("click", function(event) {
-        event.preventDefault();
-        popup.classList.add("feedback-show");
-        overlay.classList.add("feedback__overlay-show");
+	if (localStorage.getItem("name") && localStorage.getItem("email")) {
+		userName.value = localStorage.getItem("name");
+		userEmail.value = localStorage.getItem("email");
+		userMessage.focus();
+	}
 
-        if (storage) {
-          name.value = storage;
-          email.focus();
-        } else {
-          name.focus();
-        }
+});
 
-      });
+form.addEventListener("submit", function (event) {
 
-      close.addEventListener("click", function(event) {
-        popup.classList.remove("feedback-show");
-        overlay.classList.remove("feedback__overlay-show");
+	if (!userName.value && !userEmail.value) {
+		event.preventDefault();
+	} else {
+		localStorage.setItem("name", userName.value);
+		localStorage.setItem("email", userEmail.value);
+	}
 
-      });
+});
 
-      overlay.addEventListener("click", function(event) {
-        popup.classList.remove("feedback-show");
-        overlay.classList.remove("feedback__overlay-show");
-      });
+close.addEventListener("click", function (event) {
+	popup.classList.remove("feedback-show");
+	overlay.classList.remove("feedback__overlay-show");
 
-      window.addEventListener("keydown", function(event) {
-        if (event.keyCode === 27) {
-          if (popup.classList.contains("feedback-show")) {
-            popup.classList.remove("feedback-show");
-            popup.classList.remove("feedback-error");
-            overlay.classList.remove("feedback__overlay-show");
-          }
-        }
-      });
+});
+
+overlay.addEventListener("click", function (event) {
+	popup.classList.remove("feedback-show");
+	overlay.classList.remove("feedback__overlay-show");
+});
+
+window.addEventListener("keydown", function (event) {
+	if (event.keyCode === 27) {
+		if (popup.classList.contains("feedback-show")) {
+			popup.classList.remove("feedback-show");
+			overlay.classList.remove("feedback__overlay-show");
+		}
+	}
+});
